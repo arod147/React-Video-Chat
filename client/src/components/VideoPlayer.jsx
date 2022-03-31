ea52d8e8db54190afc90970c08027af026ce6553
arod147 <code.alex.rodriguez@outlook.com>
2022-03-31 17:26:02 -0400

    Video player working!

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Grid, Typography, Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -25,8 +25,43 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const VideoPlayer = () => {
+  const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } =
+    useContext(SocketContext);
+
   const classes = useStyles();
-  return <Grid container className={classes.gridContainer}></Grid>;
+  return (
+    //Our video
+    <Grid container className={classes.gridContainer}>
+      {stream && (
+        <Paper className={classes.paper}>
+          <Grid xs={12} md={6}>
+            <Typography variant="h5">{name || "Name"}</Typography>
+            <video
+              playsInline
+              muted
+              ref={myVideo}
+              autoPlay
+              className={classes.video}
+            />
+          </Grid>
+        </Paper>
+      )}
+      // Users Video
+      {callAccepted && !callEnded && (
+        <Paper className={classes.paper}>
+          <Grid xs={12} md={6}>
+            <Typography variant="h5">{call.name || "Name"}</Typography>
+            <video
+              playsInline
+              ref={userVideo}
+              autoPlay
+              className={classes.video}
+            />
+          </Grid>
+        </Paper>
+      )}
+    </Grid>
+  );
 };
 
 export default VideoPlayer;
