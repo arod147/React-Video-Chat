84d24181cfc8fde570c4ba692adc9e3c3de51b70
arod147 <code.alex.rodriguez@outlook.com>
2022-03-31 16:46:20 -0400

    Server side and socket logic set up

---
+++ client/src/components/Options.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Options = ({ childern }) => {
+  return <div>Options{childern}</div>;
+};
+
+export default Options;
---
+++ client/src/components/VideoPlayer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Grid, Typography, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { SocketContext } from "../SocketContext";
+
+const useStyles = makeStyles((theme) => ({
+  video: {
+    width: "550px",
+    [theme.breakpoints.down("xs")]: {
+      width: "300px",
+    },
+  },
+  gridContainer: {
+    justifyContent: "center",
+    [theme.breakpoints.down("xs")]: {
+      flexDirection: "column",
+    },
+  },
+  paper: {
+    padding: "10px",
+    border: "2px solid black",
+    margin: "10px",
+  },
+}));
+
+const VideoPlayer = () => {
+  const classes = useStyles();
+  return <Grid container className={classes.gridContainer}></Grid>;
+};
+
+export default VideoPlayer;
